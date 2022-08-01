4d7edb0801322faf73afffd3caec3b5a306c94e1
tareqot23-meet <109899500+tareqot23-meet@users.noreply.github.com>
2022-08-01 18:17:45 +0300

    Update pythonReview.py

--- pythonReview.py
+++ pythonReview.py
@@ -3,14 +3,21 @@ def create_youtube_video(title,description):
 
 
 	def like(youtube video) :
-		youtube_video["likes"]+= 1
-		return youtube_video
+		if 'likes' in video:
+			youtube_video["likes"]+= 1
+			return youtube_video
 
 
 	def deslike(youtube video) :
-		youtube_video["dislikes"]+=1
-		return youtube_video
+		if 'dislikes' in video:
+			youtube_video["dislikes"]+=1
+			return youtube_video
 
 	def addComment(youtube video, username, comment):
     youtube_video["comments"][username] = comment
-    return youtube_video
+    return 
+
+
+    function= create_youtube_video()
+    for i in range(495):
+    	like(function)
